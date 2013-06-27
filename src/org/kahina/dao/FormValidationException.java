cbbd814a6dc926c30d658787c3f6f7d7be3917bb
aakahina <kaitaissi@yahoo.ca>
2013-06-26 21:20:51 -0400

    first commit

---
+++ src/org/kahina/dao/FormValidationException.java
@@ -0,0 +1,10 @@
+package org.kahina.dao;
+
+public class FormValidationException extends Exception {
+    /*
+     * Constructeur
+     */
+    public FormValidationException( String message ) {
+        super( message );
+    }
+}
